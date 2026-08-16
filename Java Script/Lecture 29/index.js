0bdd773be1de96d64c2b1fd34f0ddfd3b4f424ac
Devanshu <devanshumohriya@gmail.com>
2026-08-16 21:06:23 +0530

    Update index.js

--- Java Script/Lecture 29/index.js
+++ Java Script/Lecture 29/index.js
@@ -1,7 +1,20 @@
-console.log("Hello World");
-console.log(4+4);
-age=21;
-// console.log(age);
-name="Devanshu";
-console.log(name,age);
+// console.log("Hello World");
+// console.log(4+4);
+// age=21;
+// // console.log(age);
+// name="Devanshu";
+// console.log(name,age);
+
+// var let and const
+
+// var age=21;
+// var age=22;
+// console.log(age)
+
+
+let name="Devanshu"
+// let name="Gulshan"
+name="Gulshan"
+
+console.log(name);
 
