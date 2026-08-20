7ddb9e4a79fc66209065fc9e6e2e9a873b0e8cbf
Devanshu <141940273+Devanshu-uu@users.noreply.github.com>
2026-08-20 23:07:02 +0530

    Update index.js

--- Java Script/Lecture 30 (Operators_Conditional_Loops)/index.js
+++ Java Script/Lecture 30 (Operators_Conditional_Loops)/index.js
@@ -1,7 +1,7 @@
-console.log("Hello World");
+// console.log("Hello World");
 
-let num1=2;
-let num2=4;
+// let num1=2;
+// let num2=4;
 // console.log(num1+num2)
 // console.log(num1-num2)
 // console.log(num1*num2)
@@ -10,7 +10,34 @@ let num2=4;
 // console.log(num1**num2)
 
 
-console.log(++num1)
-console.log(num1++)
+// console.log(++num1)
+// console.log(num1++)
 
-console.log("5"===5)
+// console.log("5"===5)
+
+const isLoggedin=false;
+
+if(isLoggedin){
+    console.log("Your are logged in ");
+
+}
+else{
+    console.log("Your are not logged in ");
+
+}
+
+const day="tue";
+switch(day){
+    case "mon":
+        console.log("It is monday");
+        break;
+        case "tue":
+            console.log("It is tuesday");
+            break;
+            case "wed":
+                console.log("It is wednesday");
+                break;
+                case "thus":
+                    console.log("It is thusday");
+                    break;
+}
