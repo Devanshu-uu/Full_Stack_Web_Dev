cb147431d9009030f46b1a9c5ddedcbe30495b5d
Devanshu <141940273+Devanshu-uu@users.noreply.github.com>
2026-08-22 02:58:42 +0530

    Update index.js

--- Java Script/Lecture 31 (Loops)/index.js
+++ Java Script/Lecture 31 (Loops)/index.js
@@ -1,9 +1,9 @@
-for (let i=1;i<=5;i++){
-    console.log(i);
-}
-// a=1;
-// while(a<=10){
-//     console.log(a);
-//     a++;
+// for (let i=1;i<=5;i++){
+//     console.log(i);
 // }
+let i=1;
+while(i<=5){
+    console.log(i);
+    i++;
+}
  
