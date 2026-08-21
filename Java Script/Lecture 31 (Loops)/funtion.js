9fedb07012033286ba29f38e4ca761ba2928673f
Devanshu <141940273+Devanshu-uu@users.noreply.github.com>
2026-08-22 03:34:08 +0530

    s

---
+++ Java Script/Lecture 31 (Loops)/funtion.js
@@ -0,0 +1,12 @@
+// function total(){
+//     console.log("Hii");
+
+// }
+
+// total()
+function total( studentname, math ,sci){
+    console.log(`${studentname} total marks: `,math+sci);
+
+}
+
+total("dev" ,23,64)
--- Java Script/Lecture 31 (Loops)/index.js
+++ Java Script/Lecture 31 (Loops)/index.js
@@ -1,13 +1,36 @@
 // for (let i=1;i<=5;i++){
 //     console.log(i);
 // }
-let i=1;
-// while(i<=5){
+// let i=1;
+// // while(i<=5){
+// //     console.log(i);
+// //     i++;
+// // }
+//  do {
 //     console.log(i);
-//     i++;
-// }
- do {
-    console.log(i);
-    i++
+//     i++
     
- } while (i<=5);
+//  } while (i<=5);
+
+
+//  for (let i=1;i<=10;i++){
+//     if(i===3 || i===7 || i===9){
+//         continue;
+        
+//     }
+//     console.log(i);
+ 
+//  }
+//  for (let i=1;i<=5;i++){
+//     if(i==3){
+//         break
+        
+//     }
+//     console.log(i);
+
+//  }
+
+num=5
+for (let i=1;i<=10;i++){
+    console.log(num*i);
+}
