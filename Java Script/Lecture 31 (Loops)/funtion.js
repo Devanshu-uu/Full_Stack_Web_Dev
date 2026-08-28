01457e3e8de7e236371d2f7601835b2515fe945e
Devanshu <141940273+Devanshu-uu@users.noreply.github.com>
2026-08-29 00:09:45 +0530

    Update funtion.js

--- Java Script/Lecture 31 (Loops)/funtion.js
+++ Java Script/Lecture 31 (Loops)/funtion.js
@@ -66,7 +66,7 @@
 // console.log(add(1,2));
 // let sub = (num1,num2) =>{
 //     return num1-num2
-// }
+// }        
 // console.log(add(1,2));
 // console.log(sub(1,2));
 
@@ -80,13 +80,18 @@
 
 
 
-function change(Number){
-    if(Number <0){
-        Number=Number/-1;
-    return Number
-    
-    
-    }
+// function convertnumbertopositive(Number){
+//     if(Number <0){
+//         Number=Number*-1;
+//     }
+//     return Number;
+// }
+
+// console.log(convertnumbertopositive(-345));
+
+let min=1,max=6;
+function dice(){
+    console.log(Math.floor(Math.random()*(max-min+1)+min));
 }
 
-console.log(change(345));
+dice()
