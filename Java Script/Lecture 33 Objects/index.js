ae2a5280c14857856e981d5d73b37535eee3f315
Devanshu <141940273+Devanshu-uu@users.noreply.github.com>
2026-09-02 02:43:11 +0530

    Create index.js

---
+++ Java Script/Lecture 33 Objects/index.js
@@ -0,0 +1,45 @@
+let product={
+    name:"Iphone",
+    price: 323233,
+    rating:7.3,
+    discount:43,
+    'reviews':32,
+    32:32,
+    printhi:function (){
+        console.log(this.discount);
+
+    },
+    math1(){
+
+    }
+}
+
+let product1=["iphone","red"]
+
+// console.log(product.name);
+// console.log(product[32]);
+// console.log(product["name"]);
+
+// let a=product.printhi()
+
+// console.log(Object.keys(product));
+// console.log(Object.values(product));
+// console.log(Object.entries(product));
+
+// for(value of product1){
+//     console.log(value);
+// }
+
+// for(value in product){
+//     console.log(value);
+// }
+
+for(value in product1){
+    console.log(value);
+}
+
+// product1.forEach(function(value){
+//     console.log(value);
+// })
+
+de
